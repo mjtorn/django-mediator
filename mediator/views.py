ab7e45d3cea364a6906cb8fdaca274bf030cd44e
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-05 16:18:40 +0000

    View-let returning only context (hackish..)

--- mediator/views.py
+++ mediator/views.py
@@ -1,6 +1,33 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django.http import HttpResponse
 
+from mediator import forms as mediator_forms
+
+def incoming_message(request):
+    """
+    """
+
+    type_ = request.POST.get('type', 'sms')
+    if type_ == 'mms':
+        raise NotImplementedError('MMS not supported yet')
+
+    return incoming_sms(request)
+
+def incoming_sms(request):
+    data = request.POST.copy() or None
+    sms_form = mediator_forms.SmsForm(data)
+
+    sms = None
+    if sms_form.is_bound and sms_form.is_valid():
+        sms = sms_form.save()
+
+    context = {
+        'sms_form': sms_form,
+        'sms': sms,
+    }
+
+    return context
 
 # EOF
 
