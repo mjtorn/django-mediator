a3bea8647d49b841913e0f16044c695678605008
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-05 15:56:03 +0000

    Form, ModelForm for Sms

--- mediator/forms.py
+++ mediator/forms.py
@@ -1,6 +1,12 @@
 # vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
 
+from django import forms
 
+from mediator import models as mediator_models
+
+class SmsForm(forms.ModelForm):
+    class Meta:
+        model = mediator_models.Sms
 
 # EOF
 
