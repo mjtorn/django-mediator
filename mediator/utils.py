31a8ba21aaa886fb043d013498f66c524e4331d8
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-05 16:32:03 +0000

    All sorts of keywords for sms, like price and operator

--- mediator/utils.py
+++ mediator/utils.py
@@ -2,8 +2,23 @@
 
 from lxml import etree
 
-def create_return(content):
+def create_return(content, numberto=None, numberfrom=None, operator=None, price=None, delivery_req_url=None):
     root = etree.Element('sms')
+
+    if numberto:
+        root.set('numberto', numberto)
+
+    if numberfrom:
+        root.set('numberfrom', numberfrom)
+
+    if operator:
+        root.set('operator', operator)
+
+    if price:
+        root.set('price', price)
+
+    if delivery_req_url:
+        root.set('delivery_req_url', delivery_req_url)
 
     message_node = etree.SubElement(root, 'message')
     message_node.text = etree.CDATA(content)
