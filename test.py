901378b7870c1c9894778fa7bae1383548eefce7
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-05 16:43:19 +0000

    Test cases

--- test.py
+++ test.py
@@ -15,5 +15,13 @@ if __name__ == '__main__':
     doc = utils.create_return('Vastaus menee goatseen', delivery_req_url='http://goatse.cx/', price='900')
     print etree.tostring(doc, xml_declaration=True, encoding='utf-8', pretty_print=True)
 
+    try:
+        err = utils.create_error('This will fail', 'satan')
+    except TypeError, msg:
+        print 'successfully caught %s\n' % msg
+
+    doc = utils.create_error('Stupid user fault', 'user')
+    print etree.tostring(doc, xml_declaration=True, encoding='utf-8', pretty_print=True)
+
 # EOF
 
