10de274b372f82830b65f6fb0677db84784ea531
Markus Törnqvist <mjt@fad3.fadgames.com>
2009-05-05 16:32:18 +0000

    More bullshit test cases

--- test.py
+++ test.py
@@ -6,7 +6,13 @@ from mediator import utils
 from lxml import etree
 
 if __name__ == '__main__':
-    doc = utils.create_return('test')
+    doc = utils.create_return('Kala on punainen viisaus')
+    print etree.tostring(doc, xml_declaration=True, encoding='utf-8', pretty_print=True)
+
+    doc = utils.create_return(u'Tänään sait kuudeksi kuukaudeksi vippiä prkl', price='900')
+    print etree.tostring(doc, xml_declaration=True, encoding='utf-8', pretty_print=True)
+
+    doc = utils.create_return('Vastaus menee goatseen', delivery_req_url='http://goatse.cx/', price='900')
     print etree.tostring(doc, xml_declaration=True, encoding='utf-8', pretty_print=True)
 
 # EOF
